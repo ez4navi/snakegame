844df519026b97627a276efdbc0aa1bc317620c8
ez4navi <Ilya.Lisovski@yandex.ru>
2019-07-17 17:44:06 +0300

    fix: food range, add snake

--- js/game.js
+++ js/game.js
@@ -12,8 +12,8 @@ let box = 32;
 let score = 0;
 
 let food = {
-    x: Math.floor(Math.random() * 17 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 1) * box,
+    x: Math.floor((Math.random() * 17 + 1)) * box,
+    y: Math.floor((Math.random() * 15 + 3)) * box,
 };
 
 let snake = [];
@@ -26,6 +26,11 @@ function drawGame() {
     ctx.drawImage(ground, 0, 0);
 
     ctx.drawImage(foodImg, food.x, food.y);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
 
 let game = setInterval(drawGame, 100);
